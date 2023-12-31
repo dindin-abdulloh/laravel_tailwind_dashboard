b1ba175920d1cef040ba426c60dfda5b744486d8
dindin <dindinabduloh.jbs@gmail.com>
2023-12-31 23:12:11 +0700

    add chart

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,7 +16,7 @@
         <div class="container flex flex-wrap">
             <div class="w-full md:w-1/2 lg:w-1/2 xl:w-1/2 mb-3 md:pr-2 lg:pr-2 xl:pr-2">
                 <div class="chart-container">
-                    <canvas id="salesChart"></canvas>
+                    <canvas id="multiAxisLineChart"></canvas>
                 </div>
             </div>
 
@@ -27,8 +27,6 @@
             </div>
         </div>
 
-
-
     </div>
 </div>
 @endsection
@@ -36,29 +34,47 @@
 @parent
     <script>
          $(document).ready(function () {
-            // Kolom kiri: Chart trafik penjualan
-            var salesCtx = $('#salesChart')[0].getContext('2d');
-            var salesData = {
+            // Kolom kiri: Multi-axis Line Chart
+            var multiAxisLineCtx = $('#multiAxisLineChart')[0].getContext('2d');
+            var multiAxisLineData = {
                 labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May'],
-                datasets: [{
-                    label: 'Monthly Sales',
-                    data: [50, 75, 120, 90, 110],
-                    backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
+                datasets: [
+                    {
+                        label: 'Monthly Sales',
+                        data: [50, 75, 120, 90, 110],
+                        borderColor: 'rgba(75, 192, 192, 1)',
+                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                        yAxisID: 'y1',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Monthly Profit',
+                        data: [20, 30, 40, 25, 35],
+                        borderColor: 'rgba(255, 99, 132, 1)',
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        yAxisID: 'y2',
+                        borderWidth: 1
+                    }
+                ]
             };
-            var salesOptions = {
+            var multiAxisLineOptions = {
                 scales: {
-                    y: {
+                    y1: {
+                        type: 'linear',
+                        position: 'left',
+                        beginAtZero: true
+                    },
+                    y2: {
+                        type: 'linear',
+                        position: 'right',
                         beginAtZero: true
                     }
                 }
             };
-            var salesChart = new Chart(salesCtx, {
-                type: 'bar',
-                data: salesData,
-                options: salesOptions
+            var multiAxisLineChart = new Chart(multiAxisLineCtx, {
+                type: 'line',
+                data: multiAxisLineData,
+                options: multiAxisLineOptions
             });
 
             // Kolom kanan: Chart 5 produk terlaris
